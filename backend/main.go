3c7da2fea33ac045d44b1a97b25db8a0255d1d1b
shuji19960425 <121063604+shuji19960425@users.noreply.github.com>
2025-02-11 13:51:27 +0900

    DBからユーザー情報の取得と登録処理を追加

--- backend/main.go
+++ backend/main.go
@@ -1,20 +1,63 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 
 	"github.com/gin-gonic/contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+var err error
+
+type User struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 func main() {
+	// DBへ接続
+	dsn := "host=db user=user password=password dbname=mydb port=5432 sslmode=disable"
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+	}
+
+	// マイグレーション
+	db.AutoMigrate(&User{})
+
 	r := gin.Default()
 
 	r.Use(cors.Default())
 
 	// シンプルなAPI
-	r.GET("/api/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Hello from Go!"})
+	r.GET("/api/users", func(c *gin.Context) {
+		var users []User
+		if err := db.Find(&users).Error; err != nil {
+			c.JSON(500, gin.H{"message": "Faild to fetch users"})
+
+			return
+		}
+
+		c.JSON(200, users)
+	})
+
+	r.POST("/api/users", func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(400, gin.H{"message": "Invalid input"})
+
+			return
+		}
+		if err := db.Create(&user).Error; err != nil {
+			c.JSON(500, gin.H{"message": "Failed to create user"})
+
+			return
+		}
+		c.JSON(201, user)
 	})
 
 	// 8080ポートで起動
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,35 @@
 import { useEffect, useState } from "react";
 
+interface User {
+  id: number;
+  name: string;
+  age: number;
+}
+
 function App() {
-  const [message, setMessage] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/hello")
+    fetch("http://localhost:8080/api/users")
       .then((res) => res.json())
-      .then((data) => setMessage(data.message))
+      .then((data) => setUsers(data))
       .catch((err) => console.error(err));
   }, []);
 
   return (
     <div>
-      <h1>React + Go + Docker</h1>
-      <p>API Response: {message}</p>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              {user.name} - {user.age} years old
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App;
