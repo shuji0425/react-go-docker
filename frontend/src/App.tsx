d7d1c631b3656a8a4364c4e41ed9965899c815fa
shuji19960425 <121063604+shuji19960425@users.noreply.github.com>
2025-02-12 23:37:51 +0900

    エラー修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import UserList from "./components/UserList";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div>
-        <h1>ユーザーマネジメントアプリ</h1>
-        <Routes>
-          <Route path="/" element={<UserList />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <h1>ユーザーマネジメントアプリ</h1>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
